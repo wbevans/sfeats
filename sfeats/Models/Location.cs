fbd954893259587477f586dd94479cbec0a78b07
wbevans <wbevans@nullified.net>
2022-03-13 16:00:15 -0500

    Remove unused defaultvalue attributes

--- sfeats/Models/Location.cs
+++ sfeats/Models/Location.cs
@@ -1,17 +1,14 @@
 ﻿using BingMapsRESTToolkit;
 using Newtonsoft.Json;
-using System.ComponentModel;
 
 namespace sfeats.Models
 {
     public class Location
     {
 
-        [DefaultValue(37.80499678)]
         [JsonProperty("latitude")]
         public double Latitude { get; set; }
 
-        [DefaultValue(-122.409331696)]
         [JsonProperty("longitude")]
         public double Longitude { get; set; }
 
