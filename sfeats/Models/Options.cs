3a89048161d8e86fc26b74d9168162c893889d7d
wbevans <wbevans@nullified.net>
2022-03-13 16:02:41 -0500

    Removed unused defaultvalue attributes

--- sfeats/Models/Options.cs
+++ sfeats/Models/Options.cs
@@ -1,22 +1,15 @@
-﻿using BingMapsRESTToolkit;
-using System.ComponentModel;
-
-namespace sfeats.Models
+﻿namespace sfeats.Models
 {
     public class Options
     {
-        [DefaultValue("datasf")]
         public string DataProvider { get; set; }
 
         public Location Origin { get; set; }
 
-        [DefaultValue("Walking")]
         public string TravelModeType { get; set; }
 
-        [DefaultValue("Miles")]
         public string DistanceUnitType { get; set; }
         
-        [DefaultValue(10)]
         public int Count { get; set; }
 
     }
