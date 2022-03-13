49391118fca36ad79b62625d47b2bd8b61c0db20
wbevans <wbevans@nullified.net>
2022-03-13 14:32:15 -0500

    Updated unit tests

---
+++ SFEatsUnitTests/FacilityProviderUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sfeats.Services;
+
+namespace SFEatsUnitTests
+{
+    [TestClass]
+    public class FacilityProviderUnitTests
+    {
+        [TestMethod]
+        public void CsvTest()
+        {
+            CSVFacilityProviderService service = new CSVFacilityProviderService();
+
+            var results = service.GetFacilitiesAsync().Result;
+
+            Assert.AreEqual(619, results.Count);
+            Assert.AreEqual("Pipo's Grill", results[0].Applicant);
+        }
+
+        [TestMethod]
+        public void DataSfTest()
+        {
+            DataSFFacilityProviderService service = new DataSFFacilityProviderService();
+            var results = service.GetFacilitiesAsync().Result;
+
+            Assert.IsTrue(results.Count > 450);
+        }
+    }
+}
